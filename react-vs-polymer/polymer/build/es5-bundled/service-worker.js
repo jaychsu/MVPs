bda7b2c0ea969847401ad7a37c2cf39d72f966ae
Jaych Su <hi@jaych.su>
2017-06-25 01:18:14 +0800

    [react-vs-polymer] build 0.3.0

--- react-vs-polymer/polymer/build/es5-bundled/service-worker.js
+++ react-vs-polymer/polymer/build/es5-bundled/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","df6be4fd1a916e808f0a8815d33fe590"],["src/da-selector/index.html","7a0ab26be84d5a7c036487e1f7224ee0"],["src/demo-app.html","04cc619a069f954474ba7ddbc95acc19"]];
+var precacheConfig = [["index.html","df6be4fd1a916e808f0a8815d33fe590"],["src/da-selector/index.html","1f1c9e6f7f80dbbd126c845755c5454e"],["src/demo-app.html","07c01eccbe7603c6b6c4eb95a2b12bb2"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
