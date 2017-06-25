1b67a8334c7b30cbbf2be585773fca24fa434c36
Jaych Su <hi@jaych.su>
2017-06-25 14:42:18 +0800

    [react-vs-polymer] config babel and related loader

--- react-vs-polymer/react/webpack.config.js
+++ react-vs-polymer/react/webpack.config.js
@@ -6,4 +6,10 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'build'),
   },
+  module: {
+    rules: [
+      { test: /\.css$/, use: 'css-loader', exclude: /node_modules/ },
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+    ],
+  },
 }
