fffb5025f2c9a3ce73bc09233418821af0050e87
Jaych Su <hi@jaych.su>
2017-08-04 22:50:55 +0800

    Import html structure into vue

--- vue/component/da-selector.vue
+++ vue/component/da-selector.vue
@@ -1,4 +1,36 @@
 <template lang="pug">
+  .da-selector(
+    :class="{ 'is-expanded': isPanelVisible }"
+  )
+    .da-selector-placeholder {{ selectedOptionData.display }}
+
+    .da-selector-search-wrap
+      input.da-selector-search(
+        type="text"
+        :placeholder="selectedOptionData.display"
+      )
+      span.da-selector-search-icon
+
+    span.da-selector-triangle
+
+    .da-selector-panel(
+      :class="{ hide: !isPanelVisible }"
+    )
+      ul.da-selector-option-list
+        li.da-selector-option-item(
+          v-for="optionData in optionDataList"
+          :class="{ active: optionData.id === selectedOptionData.id }"
+        )
+          | {{ optionData.display }}
+
+    label(
+      :for="id"
+    )
+      | {{ placeholder }}
+    input(
+      type="hidden"
+      :id="id"
+    )
 </template>
 
 <script>
