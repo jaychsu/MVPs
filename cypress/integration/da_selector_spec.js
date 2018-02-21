f66c5499edd5cb1de610e849aaeba45108c1e173
Jaych Su <hi@jaych.su>
2018-02-21 11:32:29 +0800

    Setup cypress to do e2e testing

---
+++ cypress/integration/da_selector_spec.js
@@ -0,0 +1,14 @@
+Object.entries({
+  'jquery': '/origin',
+  'polymer': '/polymer/build/es5-bundled',
+  'react': '/react/dist',
+  'vue': '/vue/dist',
+}).forEach(([FRAMEWORK, PATH]) => {
+
+describe(`Test da-selector in ${FRAMEWORK}`, function () {
+  it('must render the component properly', function () {
+    expect(true).to.equal(true)
+  })
+})
+
+})
